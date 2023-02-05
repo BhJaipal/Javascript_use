c5f333989e5a36cb8cc37344992a0d955aa3fa48
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-02-05 21:20:06 +0530

    Update Codding_hub.js

--- src/js/Codding_hub.js
+++ src/js/Codding_hub.js
@@ -13,11 +13,16 @@ cook.style.display="none";
     
 
 function sUp() {
-    var elem=document.getElementById("python");
+    var elem=document.getElementById("py");
     
-    if (elem.style.height=="230px") {
-    elem.style.height="47px";}
+    if (elem.style.height== "230px") {
+        elem.style.height = "47px";
+        elem.style.overflow= "hidden";
+        elem.style.transitionDuration: "1s";
+    }
 
     else {
-    elem.style.height="230px";}
+        elem.style.height="230px";
+        elem.style= "overflow: hidden; transition-duration: 1s";
+    }
 };
