f64238e92b67461b6c70536b1ae9f1dcfdbe77f1
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-03-31 17:40:53 +0530

    Add files via upload

---
+++ AprilFool/index.js
@@ -0,0 +1,7 @@
+let btn= document.getElementById('subBtn');
+btn.addEventListener("click", function() {
+    window.alert("Hello "+ document.getElementById("nameBox").value);
+    while (true) {
+        window.alert("April Fool");
+    }
+})
