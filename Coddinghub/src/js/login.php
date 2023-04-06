ffc40a75e58686df1eb0f2a0c2b185a6d241b85c
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-04-06 07:02:55 +0530

    Update login.php

--- Coddinghub/src/js/login.php
+++ Coddinghub/src/js/login.php
@@ -1,31 +1,30 @@
 <?php
 session_start();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $username = $_POST['name'];
-    $email= $_POST["email"];
-    $password = $_POST['password'];
+$username = $_POST['name'];
+$email= $_POST["email"];
+$password = $_POST['password'];
 
-    $host = "localhost";
-    $user = "Jaipal";
-    $passwd = "root";
-    $dbname = "myDB";
+$host = "localhost";
+$user = "Jaipal";
+$passwd = "root";
+$dbname = "myDB";
 
-    $conn = mysqli_connect($host, $user, $passwd, $dbname);
+$conn = mysqli_connect($host, $user, $passwd, $dbname);
 
-    if (!$conn) {
-        die("Connection failed: " . mysqli_connect_error());
-    }
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
-    mysqli_query($conn, "CREATE TABLE if not exists userData(
+mysqli_query($conn, "CREATE TABLE if not exists userData(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name varchar(15),
     email varchar(20),
     passwd varchar(10)
-    )");
-    $sql = "INSERT INTO userData(name, email, passwd) VALUES('$username', '$email', '$password')";
-    $result = mysqli_query($conn, $sql);
-    mysqli_close($conn);
-}
+)");
+$sql = "INSERT INTO userData(name, email, passwd) VALUES('$username', '$email', '$password')";
+$result = mysqli_query($conn, $sql);
+mysqli_close($conn);
+
 header("Location: Codding_hub.html");
 ?>
