36b0491457fc99ece397fec5d7f2b81b420923b7
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-04-07 21:09:17 +0530

    Update Database.php

--- Coddinghub/src/js/Database.php
+++ Coddinghub/src/js/Database.php
@@ -1,10 +1,13 @@
 <?php
 $conn= mysqli_connect("localhost", $username ="Jaipal", $password="root");
+$conn->query("CREATE DATABASE data");
+$conn->query("USE data");
 $conn->query("CREATE TABLE userData(
     id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
     name varchar(15), 
     email varchar(20), 
     passwd varchar(10) 
 )");
+$conn->commit();
 $conn->close();
 ?>
