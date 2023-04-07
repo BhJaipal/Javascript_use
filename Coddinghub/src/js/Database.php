94cff8290c56d0f56ba4eff15efeea209fa83786
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-04-07 20:58:55 +0530

    Create Database.php

---
+++ Coddinghub/src/js/Database.php
@@ -0,0 +1,10 @@
+<?php
+$conn= mysqli_connect("localhost", $username ="Jaipal", $password="root");
+$conn->query("CREATE TABLE userData(
+    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+    name varchar(15), 
+    email varchar(20), 
+    passwd varchar(10) 
+)");
+$conn->close();
+?>
