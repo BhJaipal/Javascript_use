f1592854951663043197d7fce5b5f6de0bc76122
Jaipal001 <jaipalbhanwariya001@gmail.com>
2023-04-07 07:39:09 +0000

    ts1

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,114 +1,128 @@
-let num1= 0, num2=0, result=0;
-let operator= "";
-
-function one() {
-    document.getElementById('input-box').value+= document.getElementById("1").innerHTML;
-};
-function two() {
-    document.getElementById('input-box').value+= document.getElementById("2").innerHTML;
-};
-function three() {
-    document.getElementById('input-box').value+= document.getElementById("3").innerHTML;
-};
-function four() {
-    document.getElementById('input-box').value+= document.getElementById("4").innerHTML;
-};
-function five() {
-    document.getElementById('input-box').value+= document.getElementById("5").innerHTML;
-};
-function six() {
-    document.getElementById('input-box').value+= document.getElementById("6").innerHTML;
-};
-function seven() {
-    document.getElementById('input-box').value+= document.getElementById("7").innerHTML;
-};
-function eight() {
-    document.getElementById('input-box').value+= document.getElementById("8").innerHTML;
-};
-function nine() {
-    document.getElementById('input-box').value+= document.getElementById("9").innerHTML;
-};
-function zero() {
-    document.getElementById('input-box').value+= document.getElementById("0").innerHTML;
-};
-function dec() {
-    document.getElementById('input-box').value+= document.getElementById("dec").innerHTML;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var document_1 = require("document");
+var window_1 = require("window");
+var num1 = 0, num2 = 0, result = 0;
+var operator = "";
+function nums(num) {
+    document_1.default.getElementById("input-box").value += String(num);
 }
-
+function one() {
+    nums(1);
+}
+;
+function two() {
+    nums(2);
+}
+;
+function three() {
+    nums(3);
+}
+;
+function four() {
+    nums(4);
+}
+;
+function five() {
+    nums(5);
+}
+;
+function six() {
+    nums(6);
+}
+;
+function seven() {
+    nums(7);
+}
+;
+function eight() {
+    nums(8);
+}
+;
+function nine() {
+    nums(9);
+}
+;
+function zero() {
+    nums(0);
+}
+;
+function dec() {
+    nums(".");
+}
 function add() {
-    num1= Number(document.getElementById("input-box").value);
-    document.getElementById("input-box").value= "";
-    document.getElementById("input-box").placeholder= num1+ "+";
-    operator="+";
+    num1 = Number(document_1.default.getElementById("input-box").value);
+    document_1.default.getElementById("input-box").value = "";
+    document_1.default.getElementById("input-box").placeholder = num1 + "+";
+    operator = "+";
 }
 function sub() {
-    num1= Number(document.getElementById("input-box").value);
-    document.getElementById("input-box").value= "";
-    document.getElementById("input-box").placeholder= num1+ "-";
-    operator="-";
+    num1 = Number(document_1.default.getElementById("input-box").value);
+    document_1.default.getElementById("input-box").value = "";
+    document_1.default.getElementById("input-box").placeholder = num1 + "-";
+    operator = "-";
 }
 function mul() {
-    num1= Number(document.getElementById("input-box").value);
-    document.getElementById("input-box").value= "";
-    document.getElementById("input-box").placeholder= num1+ "×";
-    operator="×";
+    num1 = Number(document_1.default.getElementById("input-box").value);
+    document_1.default.getElementById("input-box").value = "";
+    document_1.default.getElementById("input-box").placeholder = num1 + "×";
+    operator = "×";
 }
 function div() {
-    num1= Number(document.getElementById("input-box").value);
-    document.getElementById("input-box").value= "";
-    document.getElementById("input-box").placeholder= num1+ "÷";
-    operator="÷";
+    num1 = Number(document_1.default.getElementById("input-box").value);
+    document_1.default.getElementById("input-box").value = "";
+    document_1.default.getElementById("input-box").placeholder = num1 + "÷";
+    operator = "÷";
 }
-
 function eq() {
     switch (operator) {
         case '+':
-            num2= Number(document.getElementById("input-box").value);
-            result= num1+ num2;
-            document.getElementById("input-box").value= result;
-            num1= result;
+            num2 = Number(document_1.default.getElementById("input-box").value);
+            result = num1 + num2;
+            document_1.default.getElementById("input-box").value = result;
+            num1 = result;
             break;
         case "-":
-            num2= Number(document.getElementById("input-box").value);
-            result= num1- num2;
-            document.getElementById("input-box").value= result;
-            num1= result;
+            num2 = Number(document_1.default.getElementById("input-box").value);
+            result = num1 - num2;
+            document_1.default.getElementById("input-box").value = result;
+            num1 = result;
             break;
         case '×':
-            num2= Number(document.getElementById("input-box").value);
-            result= num1* num2;
-            document.getElementById("input-box").value= result;
-            num1= result;
+            num2 = Number(document_1.default.getElementById("input-box").value);
+            result = num1 * num2;
+            document_1.default.getElementById("input-box").value = result;
+            num1 = result;
             break;
         case '÷':
-            num2= Number(document.getElementById("input-box").value);
-            if (num2==0) {
-                window.alert("Cannot divide by Zero");
+            num2 = Number(document_1.default.getElementById("input-box").value);
+            if (num2 == 0) {
+                window_1.default.alert("Cannot divide by Zero");
             }
             else {
-                result= num1/ num2;
-                document.getElementById("input-box").value= result;
-                num1= result;
+                result = num1 / num2;
+                document_1.default.getElementById("input-box").value = result;
+                num1 = result;
             }
             break;
     }
 }
-
 function clear() {
-    document.getElementById("input-box").value= "";
-    document.getElementById("input-box").placeholder="";
-    num1= 0; operator="";
+    document_1.default.getElementById("input-box").value = "";
+    document_1.default.getElementById("input-box").placeholder = "";
+    num1 = 0;
+    operator = "";
 }
 function del() {
-    let inp= document.getElementById("input-box").value;
-    let j=inp;
-    inp="";
-    for (var i = 0; i < j.length-1; i++) {
-        inp+= j[i];
+    var inp = document_1.default.getElementById("input-box").value;
+    var j = inp;
+    inp = "";
+    for (var i = 0; i < j.length - 1; i++) {
+        inp += j[i];
     }
-    document.getElementById("input-box").value= inp;
+    document_1.default.getElementById("input-box").value = inp;
 }
 function neg() {
-    let input= -(Number(document.getElementById("input-box").value));
-    document.getElementById("input-box").value= String(input);
+    var input = -(Number(document_1.default.getElementById("input-box").value));
+    document_1.default.getElementById("input-box").value = String(input);
 }
