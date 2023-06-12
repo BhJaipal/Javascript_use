3c29eb5b6ab2bb56318f97e03f68e609f664638d
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-12 14:30:52 +0530

    Add files via upload

---
+++ Calculator/index.tsx
@@ -0,0 +1,11 @@
+
+import App from "./App.jsx";
+import * as React from "react";
+import * as ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+);
