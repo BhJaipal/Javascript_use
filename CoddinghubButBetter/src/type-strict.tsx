3d36234cba3165ae701e878afafbf20b5747b153
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-15 09:21:08 +0530

    Update CoddinghubButBetter/src/type-strict.tsx

--- CoddinghubButBetter/src/type-strict.tsx
+++ CoddinghubButBetter/src/type-strict.tsx
@@ -1,4 +1,4 @@
-import * as Wi
+import * as Window 
 const alphabets= ("ABCDEFGHIJKLMNOPQRSTUVWXYZ+×÷_=/").split("");
 export default function strToNum(string: string) {
     let argList = string.toUpperCase().split("");
