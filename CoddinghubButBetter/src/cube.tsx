12e258fcf0b586000cda9ce726fec92a23d2966a
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-15 10:12:06 +0530

    Update CoddinghubButBetter/src/cube.tsx

--- CoddinghubButBetter/src/cube.tsx
+++ CoddinghubButBetter/src/cube.tsx
@@ -5,7 +5,7 @@ import strToNum from "./type-strict.jsx";
 
 export default class Cube{
     calculate() {
-        let cube= new ThreeD.Cuboid(strToNum($("side").val));
+        let cube= new ThreeD.Cuboid(strToNum($("#side").val));
         let out= `Total Surface Area: ${cube.TotalSurfaceArea()}\nLateral Surface Area: ${cube.LateralSurfaceArea()}\nVolume: ${cube.Volume()}\nLength of Diagonal: ${cube.Diagonal()}`;
         setOutput(out);
     }
