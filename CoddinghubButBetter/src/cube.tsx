a3a50e9eaf370b52861a992c6fcc8678b4eedcc3
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-15 09:36:43 +0530

    Update CoddinghubButBetter/src/cube.tsx

--- CoddinghubButBetter/src/cube.tsx
+++ CoddinghubButBetter/src/cube.tsx
@@ -11,7 +11,7 @@ export default class Cube{
     }
     render () {
         return (
-            <div class="cub">
+            <div class="cube ">
                 
             </div>
         )
