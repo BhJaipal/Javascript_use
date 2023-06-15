79cd87f4c0779267786729d1762d2948d097cb4f
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-15 10:12:47 +0530

    Update CoddinghubButBetter/src/cube.tsx

--- CoddinghubButBetter/src/cube.tsx
+++ CoddinghubButBetter/src/cube.tsx
@@ -13,7 +13,7 @@ export default class Cube{
         return (
             <div className="cube">
                 <Label htmlFor="side">Side: </Label>
-                Input
+                <Input:month></Input:month>
             </div>
         )
     }
