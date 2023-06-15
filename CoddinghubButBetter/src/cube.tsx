489abe0564362d3ca6d8e4e32bbf6c01cc6dd2b7
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-15 09:33:50 +0530

    Update CoddinghubButBetter/src/cube.tsx

--- CoddinghubButBetter/src/cube.tsx
+++ CoddinghubButBetter/src/cube.tsx
@@ -5,6 +5,6 @@ import strToNum from "./type-strict.jsx";
 
 export default class Cube{
     calculate() {
-        let cube= new ThreeD.Cuboid(strToNum)
+        let cube= new ThreeD.Cuboid(strToNum())
     }
 }
