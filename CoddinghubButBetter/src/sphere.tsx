8eb7d8b5da00b271aadc74db13aa4424d64f1b72
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:14:23 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -2,11 +2,12 @@ import * as React from "react";
 import $ from "./jquery.js";
 import ThreeD from "./3d.tsx"
 
+
 export default class Sphere {
     calculate() {
         let sphere = new ThreeD.Sphere(strToNum($("#rad").val));
         let out = `Total Surface Area: ${sphere.TotalSurfaceArea()}\nVolume: ${sphere.Volume()}`;
-        setOutput(out)
+        setOutput(out);
     }
     render () {
         return (
