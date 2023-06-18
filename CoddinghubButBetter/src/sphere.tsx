ee3be373df778945255c7471b69c428db8e65d25
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 10:28:48 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -3,7 +3,7 @@ import $ from "./jquery.js";
 
 export default class Sphere{
     calculate() {
-        let sphere= new Thre
+        let sphere= new ThreeD.
         let out= `Total Surface Area: ${sphere.TotalSurfaceArea()}\nVolume: ${sphere.Volume()}`;
     }
 }
