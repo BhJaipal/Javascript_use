91dd3ad9611c290c4717061238b630f3bd98b017
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:14:48 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -11,7 +11,7 @@ export default class Sphere {
     render () {
         return (
             <div className="sphere">
-                input
+                <I></I>nput
             </div>
         )
     }
