534f56e7fdc12ce4ef084cbab94127cabd02bc47
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:14:33 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -2,7 +2,6 @@ import * as React from "react";
 import $ from "./jquery.js";
 import ThreeD from "./3d.tsx"
 
-
 export default class Sphere {
     calculate() {
         let sphere = new ThreeD.Sphere(strToNum($("#rad").val));
