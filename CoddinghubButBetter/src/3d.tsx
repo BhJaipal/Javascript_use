cd620c53b3f3e8c03ac774498969e34092159e6c
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:04:26 +0530

    Update CoddinghubButBetter/src/3d.tsx

--- CoddinghubButBetter/src/3d.tsx
+++ CoddinghubButBetter/src/3d.tsx
@@ -188,7 +188,7 @@ export default const ThreeD= {
       );
     }*/
   },
-  class Cone implements Shape {
+  class implements Shape {
     constructor(radius: number, height: number) {
       this.radius = radius;
       this.height = height;
