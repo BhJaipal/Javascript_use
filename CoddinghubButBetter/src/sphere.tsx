e235c787a1c1401cd71b425f3b4dc8d2bc0eca27
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 10:28:14 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -3,6 +3,6 @@ import $ from "./jquery.js";
 
 export default class Sphere{
     calculate() {
-        let out= `Total Surface Area: ${sphere.TotalSurfaceArea()}\nVolume: ${sphere .Volume()}`;
+        let out= `Total Surface Area: ${sphere.TotalSurfaceArea()}\nVolume: ${sphere.Volume()}`;
     }
 }
