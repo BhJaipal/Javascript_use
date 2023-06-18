8957c2ceb9bfdb7a2923d1d22bc11c155e9fe5ca
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 10:26:11 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -3,6 +3,7 @@ import $ from "./jquery.js";
 
 export default class Sphere{
     calculate() {
-
+        let out= `Total Surface Area: ${cuboid.TotalSurfaceArea()}\nLateral Surface Area: ${cuboid.LateralSurfaceArea()}\nVolume: ${cuboid.Volume()}\nLength of Diagonal: ${cuboid.Diagonal()}`;
+        
     }
 }
