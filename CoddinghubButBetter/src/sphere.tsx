378ee3e9e650a218b8760fd955fee428ff82be15
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:15:50 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -11,7 +11,7 @@ export default class Sphere {
     render () {
         return (
             <div className="sphere">
-                <Label htmlFor="rad"></Label>
+                <Label htmlFor="rad">Ra</Label>
                 <Input:number id="rad"></Input:number>
             </div>
         )
