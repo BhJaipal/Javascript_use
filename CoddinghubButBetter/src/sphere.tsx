c3fdf1fcd2eff38a1881db0b50ed546575fa9d7f
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-18 11:05:31 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -4,7 +4,7 @@ import ThreeD from "./3d.jsx"
 
 export default class Sphere {
     calculate() {
-        let sphere = new ThreeD.Sphere()
+        let sphere = new ThreeD.Sphere)
         let out = `Total Surface Area: ${sphere.TotalSurfaceArea()}\nVolume: ${sphere.Volume()}`;
     }
 }
