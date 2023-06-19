d7ff944d3b89aac8248325f41887b41231e3f888
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-19 11:57:48 +0530

    Update CoddinghubButBetter/src/sphere.tsx

--- CoddinghubButBetter/src/sphere.tsx
+++ CoddinghubButBetter/src/sphere.tsx
@@ -13,7 +13,7 @@ export default class Sphere {
             <div className="sphere">
                 <Label htmlFor="rad">Radius: </Label>
                 <Input:number id="rad"></Input:number>
-                <Button:button 9nClick={calculate}>Submit</Button:button>
+                <Button:button onClick={calculate}>Submit</Button:button>
             </div>
         )
     }
