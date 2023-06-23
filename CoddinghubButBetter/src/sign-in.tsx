b37c55f47f7001f024677cf57fff56ae51cbef0f
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-23 08:25:31 +0530

    Update CoddinghubButBetter/src/sign-in.tsx

--- CoddinghubButBetter/src/sign-in.tsx
+++ CoddinghubButBetter/src/sign-in.tsx
@@ -42,7 +42,7 @@ export default function SignIn() {
                 </div>
                 <div className="form-check text-start my-3">
                     <Input:checkbox className="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault" />
-                        <Label className="form-check-Label" for="flexCheckDefault">
+                        <Label className="form-check-Label" htmlfor="flexCheckDefault">
                             Remember me
                         </Label>
                 </div>
