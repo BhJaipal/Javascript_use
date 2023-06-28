05a1381e7c72afac1fa5b2cda1539d7a196fb092
Jaipal001 <105307427+BhJaipal@users.noreply.github.com>
2023-06-28 10:37:36 +0530

    Update CoddinghubButBetter/src/Home.tsx

--- CoddinghubButBetter/src/Home.tsx
+++ CoddinghubButBetter/src/Home.tsx
@@ -3,6 +3,17 @@ import React from 'react';
 
 export default function Home() {
     return(
-        rf
+        import React from 'react'
+        
+        const Home = () => {
+          return (
+            <div>
+              
+            </div>
+          )
+        }
+        
+        export default Home
+        
     )
 }
